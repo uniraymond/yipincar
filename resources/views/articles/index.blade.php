db96edb624403ef5db8dd70deadb995eae2b663e
Raymond <ifengraymond@gmail.com>
2016-08-15 13:02:18 +1200

    remove some angularjs setting

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -2,8 +2,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container container-full" ng-app="articleApp">
-        <div class="row" ng-controller="articleController">
+    <div class="container container-full">
+        <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <h1>Articles</h1>
             </div>
