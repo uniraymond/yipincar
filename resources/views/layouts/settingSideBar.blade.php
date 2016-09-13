735f9b730db730834858f12db69f6511f56c134a
uniraymond <uniraymond@gmail.com>
2016-09-14 00:54:50 +1200

    add database.php to gitignore

--- resources/views/layouts/settingSideBar.blade.php
+++ resources/views/layouts/settingSideBar.blade.php
@@ -1,7 +1,9 @@
 <div class="navbar-default sidebar" role="navigation">
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
-            <li class="list-group-item {{ (Request::is('*taboo') ? 'active' : '') }}"> <a href="{{ url('admin/taboo') }}"><i class="fa fa-files-o fa-fw"></i> 敏感词 </a></li>
+            @if((null !== Auth::user()) && Auth::user()->hasAnyRole(['super_admin', 'admin']))
+                <li class="list-group-item {{ (Request::is('*taboo') ? 'active' : '') }}"> <a href="{{ url('admin/taboo') }}"><i class="fa fa-files-o fa-fw"></i> 敏感词 </a></li>
+            @endif
             @if((null !== Auth::user()) && Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'adv_editor']))
                 <li class="list-group-item {{ (Request::is('*user') || Request::is('*profile') ? 'active' : '') }}"><a href="{{ url('admin/user') }}"> <i class="fa fa-files-o fa-fw"></i> 用户管理</a></li>
                 <li class="list-group-item {{ (Request::is('*comment') ? 'active' : '') }}"><a href="{{ url('admin/comment') }}"> <i class="fa fa-files-o fa-fw"></i> 评论管理</a></li>
