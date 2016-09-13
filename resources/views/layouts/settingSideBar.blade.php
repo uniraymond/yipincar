cd513be641081b4c6714a21cea1372b2f398d6ff
uniraymond <uniraymond@gmail.com>
2016-09-13 14:53:37 +1200

    left manu role

--- resources/views/layouts/settingSideBar.blade.php
+++ resources/views/layouts/settingSideBar.blade.php
@@ -4,9 +4,9 @@
             <li class="list-group-item {{ (Request::is('*taboo') ? 'active' : '') }}"> <a href="{{ url('admin/taboo') }}"><i class="fa fa-files-o fa-fw"></i> 敏感词 </a></li>
             @if((null !== Auth::user()) && Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'adv_editor']))
                 <li class="list-group-item {{ (Request::is('*user') || Request::is('*profile') ? 'active' : '') }}"><a href="{{ url('admin/user') }}"> <i class="fa fa-files-o fa-fw"></i> 用户管理</a></li>
+                <li class="list-group-item {{ (Request::is('*comment') ? 'active' : '') }}"><a href="{{ url('admin/comment') }}"> <i class="fa fa-files-o fa-fw"></i> 评论管理</a></li>
             @endif
             <li class="list-group-item {{ (Request::is('*tag') ? 'active' : '') }}" ><a href="{{ url('admin/tag') }}"> <i class="fa fa-files-o fa-fw"></i> 标签管理</a></li>
-            <li class="list-group-item {{ (Request::is('*comment') ? 'active' : '') }}"><a href="{{ url('admin/comment') }}"> <i class="fa fa-files-o fa-fw"></i> 评论管理</a></li>
         </ul>
     </div>
 </div>
