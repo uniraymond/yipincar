6edbfa89a005496d3b40bc4f4b6840f79182af4d
Raymond <ifengraymond@gmail.com>
2016-09-18 10:47:48 +1200

    add more on profile db table

--- database/migrations/2016_09_17_140307_create_object_types_table.php
+++ database/migrations/2016_09_17_140307_create_object_types_table.php
@@ -12,7 +12,7 @@ class CreateObjectTypesTable extends Migration
      */
     public function up()
     {
-        Schema::table('object_types', function (Blueprint $table) {
+        Schema::create('object_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('description');
--- database/migrations/2016_09_17_140439_add_additions_to_profiles_table.php
+++ database/migrations/2016_09_17_140439_add_additions_to_profiles_table.php
@@ -1,67 +1,59 @@
 <?php
-//
-//use Illuminate\Database\Schema\Blueprint;
-//use Illuminate\Database\Migrations\Migration;
-//
-//class AddAdditionsToProfilesTable extends Migration
-//{
-//    /**
-//     * Run the migrations.
-//     *
-//     * @return void
-//     */
-//    public function up()
-//    {
-//        Schema::table('profiles', function (Blueprint $table) {
-//            $table->integer('media_type_id')->references('id')->on('object_types');
-//            $table->integer('prove_id')->references('id')->on('object_types');
-//            $table->integer('recource_id')->references('id')->on('recource');
-//            $table->integer('city_id')->references('id')->on('object_types');
-//            $table->string('email');
-//            $table->string('auth_rource_id')->reference('id')->on('recource');
-//            $table->string('ass_recouse');
-//            $table->string('weixin_public_id');
-//            $table->string('media_name');
-//            $table->string('media_icon')->reference('id')->on('recource');
-//            $table->text('media_description');
-//            $table->tinyInteger('agree');
-//            $table->string('orgnise_icon')->reference('id')->on('recource');
-//            $table->string('contract_auth');
-//            $table->string('self_url');
-//            $table->string('ass_resource');
-//            $table->string('weixin_public_id');
-//            $table->string('self_media_name');
-//            $table->string('self_icon')->reference('id')->on('recource');
-//        });
-//    }
-//
-//    /**
-//     * Reverse the migrations.
-//     *
-//     * @return void
-//     */
-//    public function down()
-//    {
-//        Schema::table('profiles', function (Blueprint $table) {
-//            $table->dropColumn('media_type_id');
-//            $table->dropColumn('prove_id');
-//            $table->dropColumn('recource_id');
-//            $table->dropColumn('city_id');
-//            $table->dropColumn('email');
-//            $table->dropColumn('auth_rource_id');
-//            $table->dropColumn('ass_recouse');
-//            $table->dropColumn('weixin_public_id');
-//            $table->dropColumn('media_name');
-//            $table->dropColumn('media_icon');
-//            $table->dropColumn('media_description');
-//            $table->dropColumn('agree');
-//            $table->dropColumn('orgnise_icon');
-//            $table->dropColumn('contract_auth');
-//            $table->dropColumn('self_url');
-//            $table->dropColumn('ass_resource');
-//            $table->dropColumn('weixin_public_id');
-//            $table->dropColumn('self_media_name');
-//            $table->dropColumn('self_icon');
-//        });
-//    }
-//}
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdditionsToProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->integer('media_type_id')->references('id')->on('object_types');
+            $table->integer('prove_id')->references('id')->on('object_types');
+            $table->integer('resource_id')->references('id')->on('recource');
+            $table->integer('city_id')->references('id')->on('object_types');
+            $table->string('email');
+            $table->string('auth_resource_id')->reference('id')->on('recource');
+            $table->string('ass_resource');
+            $table->string('weixin_public_id');
+            $table->string('media_name');
+            $table->string('media_icon')->reference('id')->on('recource');
+            $table->text('media_description');
+            $table->tinyInteger('agree');
+            $table->string('orgnise_icon')->reference('id')->on('recource');
+            $table->string('contract_auth');
+            $table->string('self_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('media_type_id');
+            $table->dropColumn('prove_id');
+            $table->dropColumn('resource_id');
+            $table->dropColumn('city_id');
+            $table->dropColumn('email');
+            $table->dropColumn('auth_resource_id');
+            $table->dropColumn('ass_recource');
+            $table->dropColumn('weixin_public_id');
+            $table->dropColumn('media_name');
+            $table->dropColumn('media_icon');
+            $table->dropColumn('media_description');
+            $table->dropColumn('agree');
+            $table->dropColumn('orgnise_icon');
+            $table->dropColumn('contract_auth');
+            $table->dropColumn('self_url');
+        });
+    }
+}
