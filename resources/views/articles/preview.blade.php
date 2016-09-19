d7e1842807690baaa7573a44a1e85a38bdba3cad
uniraymond <uniraymond@gmail.com>
2016-09-19 13:28:45 +1200

    temp

---
+++ resources/views/articles/preview.blade.php
@@ -0,0 +1,11 @@
+
+<div class="" style ="height: 380px, width: 480px;">
+    <div class="title">
+        {{ $article->title }}
+    </div>
+
+    <div class="content">
+        {{ $article->content }}
+    </div>
+
+</div>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -59,7 +59,7 @@
                     </div>
                     @if ( Null !== Auth::user() && $article->created_by == Auth::user()->id || Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'main_editor', 'adv_editor']) )
                         <div class="col-lg-4 col-md-4 col-sm-4 edit_article pull-right clearfix">
-                            <a id="pv" class="inline cboxElement btn btn-primary" href="#preview">预览</a>
+                            <a id="pv" class="inline cboxElement btn btn-primary" href="http://yipin/admin/article/preview/14">预览</a>
                             @if ( Null !== Auth::user() && $article->created_by == Auth::user()->id && $article->published == 1 || Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'main_editor']))
                                 {{ link_to('admin/article/'.$article->id.'/edit', '编辑', ['class'=>'btn btn-primary']) }}
                             @endif
@@ -89,7 +89,7 @@
 <script src="{{ url('/src/js/jQuery.min.2.2.4.js') }}"></script>
 <script src="{{ url('/src/js/jquery.colorbox-min.js') }}"></script>
 <script>
-jQuery("#pv").colorbox({inline:true, href:"#preview", width:"376px", height: "667px"});
+jQuery("#pv").colorbox();
 </script>
 @endsection
 
