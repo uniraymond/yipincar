d84e0826dcbeda160bff1cc9e9a6b107e5ea8f54
Raymond <ifengraymond@gmail.com>
2016-09-24 00:43:25 +1200

    change footer icp

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="navbar navbar-default navbar-static-top">
-    <div class="col-md-12" id="footer_contents">©2016 一品汽车 备案号：<a href="http://www.beian.gov.cn/" >京备 329748723974号</a></div>
+    <div class="col-md-12" id="footer_contents">©2016 一品汽车 备案号：<a href="http://www.beian.gov.cn/" >京ICP备16047978号</a></div>
 </footer>
