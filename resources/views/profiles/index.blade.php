a21cb061850eec935d6e4913ff7c660948a8232b
uniraymond <uniraymond@gmail.com>
2016-09-27 14:40:09 +1300

    profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,7 +5,7 @@
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                点击<a href="{{ url('admin/profile/'.Auth::user()->id.'/create') }}" >跳转</a>按钮完成个人信息.
+                还没有填写个人信息，点击<a href="{{ url('admin/profile/'.Auth::user()->id.'/create') }}" >跳转</a>按钮完成个人信息.
             </div>
         </div>
     </div>
