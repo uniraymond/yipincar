29301062f5918122239dc6fdbd97de8fb8669475
uniraymond <uniraymond@gmail.com>
2016-10-09 18:25:57 +1300

    user menu

--- resources/views/layouts/userSideBar.blade.php
+++ resources/views/layouts/userSideBar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
             <li class="list-group-item {{ (Request::is('*article') ? 'active' : '') }}"> <a href="{{ url('admin/user/create') }}"><i class="fa fa-files-o fa-fw"></i> 登记 </a></li>
-            <li class="list-group-item {{ (Request::is('*advsetting') ? 'active' : '') }}"><a href="{{ url('admin/user') }}"> <i class="fa fa-files-o fa-fw"></i> 会员列表</a></li>
+            <li class="list-group-item {{ (Request::is('*advsetting') ? 'active' : '') }}"><a href="{{ url('admin/user/authEditorList') }}"> <i class="fa fa-files-o fa-fw"></i> 会员列表</a></li>
         </ul>
     </div>
 </div>
