5e4b37cb880aed0a68ddfe375894b8a6aa36f839
uniraymond <uniraymond@gmail.com>
2016-10-09 20:30:03 +1300

    article menu

--- resources/views/layouts/contentSideBar.blade.php
+++ resources/views/layouts/contentSideBar.blade.php
@@ -8,7 +8,7 @@
             @if((null !== Auth::user()) && Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'adv_editor']))
             <li class="list-group-item {{ (Request::is('*advsetting*') ? 'active' : '') }}"><a href="{{ url('admin/advsetting/list') }}"> <i class="fa fa-files-o fa-fw"></i> 广告管理</a></li>
             @endif
-            <li class="list-group-item {{ (Request::is('*activeArticle') ? 'active' : '') }}" ><a href="{{ url('admin/articles/actived') }}"> <i class="fa fa-files-o fa-fw"></i> 发布列表</a></li>
+            <li class="list-group-item {{ (Request::is('*articles/actived') ? 'active' : '') }}" ><a href="{{ url('admin/articles/actived') }}"> <i class="fa fa-files-o fa-fw"></i> 发布列表</a></li>
             @if((null !== Auth::user()) && Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'adv_editor']))
             <li class="list-group-item {{ (Request::is('*history') ? 'active' : '') }}"><a href="{{ url('admin/history') }}"> <i class="fa fa-files-o fa-fw"></i> 历史记录</a></li>
             @endif
