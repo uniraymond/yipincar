89e29d5d19672782842cf4346a3b13192b08f399
uniraymond <uniraymond@gmail.com>
2016-10-15 09:42:51 +1300

    preview

--- resources/views/articles/preview.blade.php
+++ resources/views/articles/preview.blade.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" href="{{ asset("/src/css/preview.css") }}"/>
 <div class="article-preview">
     <div class="title col-xs-12">{{ $article->title }}</div>
-    <div class="category col-xs-11">{{ $article->categories->name }}
-        <span class="authname" >{{ $article->authname ? $article->authname : $article->user_created_by->name }}</span>
-        <span class="article_publish_date">{{ $article->created_at }}</span> </div>
-    <div class="comment col-xs-1">评论{{ count($article->comments) }}</div>
+    <div class="clearfix"></div>
+    <div class="subtitle">
+        <div class="category col-xs-10">{{ $article->categories->name }}
+            <span class="authname" >{{ $article->authname ? $article->authname : $article->user_created_by->name }}</span>
+            <span class="article_publish_date">{{ $article->created_at }}</span> </div>
+        <div class="comment col-xs-2">评论{{ count($article->comments) }}</div>
+    </div>
+    <div class="clearfix"></div>
     <div class="content col-xs-12">{!! $article->content !!}</div>
 </div>
