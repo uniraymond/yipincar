5e1b593fc7818f7bf5316c3991c002c9f9039bfe
uniraymond <uniraymond@gmail.com>
2016-10-26 08:15:04 +1000

    tiny 3

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -75,7 +75,7 @@
                             {{--<label class="col-lg-12 col-md-12 col-sm-12 clearfix">内容</label>--}}
                             <div class="clearfix"></div>
                             <div class="col-md-12">
-                                <textarea class="col-lg-12 col-md-12 col-sm-12 form-control clearfix" id="content" name="content" height="50"></textarea>
+                                <textarea class="col-lg-12 col-md-12 col-sm-12 form-control clearfix maincontent" id="content" name="content" height="50"></textarea>
                                 @if ($errors->has('content'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('content') }}</strong>
@@ -137,7 +137,7 @@
         };
     </script>
 @endsection
-<script src="{{ url('/src/js/vendor/tinymce/js/tinymce/tinymce.min.js') }}"></script>
+{{--<script src="{{ url('/src/js/vendor/tinymce/js/tinymce/tinymce.min.js') }}"></script>--}}
 <script>
 //    var sampleApp = angular.module('myapp', [], function($interpolateProvider) {
 //        $interpolateProvider.startSymbol('<%');
@@ -145,45 +145,60 @@
 //    });
 </script>
 <script src="{{ url('/src/js/jQuery.min.2.2.4.js') }}" ></script>
+<script src="{{ url('/src/js/tiny3/tiny_mce.js') }}"></script>
 <script>
-    var editor_config = {
-        language:'zh_CN',
-        height: "350",
-        path_absolute : "{{ URL::to('/') }}/",
-        selector: "textarea#content",
-        plugins : 'link image imagetools preview',
-        menubar: false,
-        toolbar: 'undo redo | image | removeformat | bold italic underline strikethrough | alignleft aligncenter alignright',
-        relative_urls: false,
-        removeformat: [
-            {selector: 'b,strong,em,i,font,u,strike', remove : 'all', split : true, expand : false, block_expand: true, deep : true},
-            {selector: 'span', attributes : ['style', 'class'], remove : 'empty', split : true, expand : false, deep : true},
-            {selector: '*', attributes : ['style', 'class'], split : false, expand : false, deep : true}
-        ],
-        file_browser_callback_types: 'image media',
-        file_browser_callback : function(field_name, url, type, win) {
-            var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-            var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
-            var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-            if (type == 'image') {
-                cmsURL = cmsURL + "&type=Images";
-            } else {
-                cmsURL = cmsURL + "&type=Files";
-            }
+    tinyMCE.init({
+        mode : "textareas",
+        editor_selector: "maincontent",
+        plugins : "openmanager",
+        file_browser_callback: "openmanager",
+        open_manager_upload_path: 'uploads/',
+        theme_advanced_buttons1 : "openmanager",
+        theme : "advanced",
+        theme_advanced_toolbar_location : "top"
+    });
 
-            tinyMCE.activeEditor.windowManager.open({
-                file : cmsURL,
-                title : 'Filemanager',
-                width : x * 0.8,
-                height : y * 0.8,
-                resizable : "yes",
-                close_previous : "no"
-            });
-        }
-    };
+</script>
+<script>
+    {{--var editor_config = {--}}
+        {{--language:'zh_CN',--}}
+        {{--height: "350",--}}
+        {{--path_absolute : "{{ URL::to('/') }}/",--}}
+        {{--selector: "textarea#content",--}}
+        {{--plugins : 'link image imagetools preview',--}}
+        {{--menubar: false,--}}
+        {{--toolbar: 'undo redo | image | removeformat | bold italic underline strikethrough | alignleft aligncenter alignright',--}}
+        {{--relative_urls: false,--}}
+        {{--removeformat: [--}}
+            {{--{selector: 'b,strong,em,i,font,u,strike', remove : 'all', split : true, expand : false, block_expand: true, deep : true},--}}
+            {{--{selector: 'span', attributes : ['style', 'class'], remove : 'empty', split : true, expand : false, deep : true},--}}
+            {{--{selector: '*', attributes : ['style', 'class'], split : false, expand : false, deep : true}--}}
+        {{--],--}}
+        {{--file_browser_callback_types: 'image media',--}}
+        {{--file_browser_callback : function(field_name, url, type, win) {--}}
+            {{--var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;--}}
+            {{--var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;--}}
 
-    tinymce.init(editor_config);
+            {{--var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;--}}
+            {{--if (type == 'image') {--}}
+                {{--cmsURL = cmsURL + "&type=Images";--}}
+            {{--} else {--}}
+                {{--cmsURL = cmsURL + "&type=Files";--}}
+            {{--}--}}
+
+            {{--tinyMCE.activeEditor.windowManager.open({--}}
+                {{--file : cmsURL,--}}
+                {{--title : 'Filemanager',--}}
+                {{--width : x * 0.8,--}}
+                {{--height : y * 0.8,--}}
+                {{--resizable : "yes",--}}
+                {{--close_previous : "no"--}}
+            {{--});--}}
+        {{--}--}}
+    {{--};--}}
+
+//    tinymce.init(editor_config);
 
     var changeFlag=false;
     //标识文本框值是否改变，为true，标识已变
