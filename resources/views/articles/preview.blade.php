638035b6698cf55381139ca5dda6a03e45989302
YuanGuan <intelinker@163.com>
2016-10-27 13:54:52 +0800

    no message

--- resources/views/articles/preview.blade.php
+++ resources/views/articles/preview.blade.php
@@ -23,7 +23,7 @@
 //        jQuery('.content p img').width(width > 800 ? 800 *0.9 : width);
 //        jQuery('.article-preview').width(width > 800 ? 800 *0.9 : width);
         jQuery('.content p img').width(width -30);
-//        jQuery('.article-preview').width(width);
+        jQuery('.article-preview').width(width -30);
 
     });
 </script>
