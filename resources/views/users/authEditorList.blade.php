7eb2924b89512ef7ca3b97e2c0650553620b2fbd
uniraymond <uniraymond@gmail.com>
2016-11-05 13:43:11 +1000

    auth editor view list

--- resources/views/users/authEditorList.blade.php
+++ resources/views/users/authEditorList.blade.php
@@ -95,7 +95,7 @@
                             </tbody>
                         </table>
                     @endif
-                    @if ( Auth::user()->hasAnyRoles(['super_admin', 'admin', 'chef_editor', 'main_editor', 'adv_editor']) )
+                    @if ( Auth::user()->hasAnyRole(['super_admin', 'admin', 'chef_editor', 'main_editor', 'adv_editor']) )
 {{--                    @if ( Auth::user()->hasRole('super_admin', 'admin') )--}}
                         <div class="col-lg-12 col-md-12 col-sm-12 clearfix">
                             <span class="totalpage pagination">共有入驻编辑：{{ ($totalUsers) }}</span>   {!! $users->links() !!}
