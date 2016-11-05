a8285f56ad908a26ce79c7fd7650b0d2d73a43c3
uniraymond <uniraymond@gmail.com>
2016-11-05 12:17:40 +1000

    add role id to comments

--- database/migrations/2016_11_05_101604_add_role_id_to_comments_table.php
+++ database/migrations/2016_11_05_101604_add_role_id_to_comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddReadedToArticlesTable extends Migration
+class AddRoleIdToCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,8 @@ class AddReadedToArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::table('articles', function (Blueprint $table) {
-            $table->integer('readed');
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('role_id');
         });
     }
 
@@ -24,8 +24,8 @@ class AddReadedToArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::table('articles', function (Blueprint $table) {
-            $table->drop('readed');
+        Schema::table('comments', function (Blueprint $table) {
+            $table->drop('role_id');
         });
     }
 }
