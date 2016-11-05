eca280b95675c981b7ebe04a9020fccb0b980ae4
uniraymond <uniraymond@gmail.com>
2016-11-05 12:05:51 +1000

    add count article numbers to articles

---
+++ database/migrations/2016_11_05_100245_add_readed_to_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReadedToArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->integer('readed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->drip('readed');
+        });
+    }
+}
