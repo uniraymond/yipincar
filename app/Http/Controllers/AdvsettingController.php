a82a8f969f9fdf9d23615025ad77742b308d4b05
uniraymond <uniraymond@gmail.com>
2016-11-24 23:08:11 +1000

    input changed

--- app/Http/Controllers/AdvsettingController.php
+++ app/Http/Controllers/AdvsettingController.php
@@ -276,14 +276,12 @@ class AdvsettingController extends Controller
 
   public function uploadimage(Request $request)
   {
-//    $this->validate($request, [
-//        'links' => 'required',
-//        'images' => 'required',
-//        'title' => 'max:23'
-//    ]);
+      $this->validate($request, [
+          'title' => 'required|max:31'
+      ]);
     $authuser = $request->user();
     $file = $request->file('images');
-      var_dump($file);exit;
+
     if (!empty($file)) {
       $fileOriginalName = $file->getClientOriginalName();
       $fileName = $this->generateImageName($file);
