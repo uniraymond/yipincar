66eb4368300f7e6e50019174fb005c08c9c3d3a4
YuanGuan <intelinker@163.com>
2016-12-14 01:15:38 +0800

    no message

--- resources/views/articles/preview.blade.php
+++ resources/views/articles/preview.blade.php
@@ -3,7 +3,7 @@
 <title>{{ $article->title }}</title>
 <link rel="stylesheet" href="{{ asset("/src/css/preview.css") }}"/>
 {{--<div class="article-preview" id="content" contenteditable="false">--}}
-<div style="margin: 35px" >
+<div style="margin: 10px" >
 
     <div class="title col-xs-12">{{ $article->title }}</div>
     <div class="clearfix"></div>
