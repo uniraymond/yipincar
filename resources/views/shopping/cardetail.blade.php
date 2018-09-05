9ae0db5e034177d2929a98faa30f496b9d5db721
intelinker <intelinker@163.com>
2018-09-05 13:31:49 +0800

    car detail blade

--- resources/views/shopping/cardetail.blade.php
+++ resources/views/shopping/cardetail.blade.php
@@ -4,17 +4,20 @@
 <link rel="stylesheet" href="{{ asset("/src/css/preview.css") }}"/>
 
 <div class="article-preview" id="prviewbody" contenteditable="false">
+    <img src="{{$car->logo}}" style="margin-top: 20px">
+        <div class="clearfix"></div>
+
     <div style="margin: 12px" >
-    <img src="/audi_a3.jpg">
+        <div class="title col-xs-12" style="margin-top: 10px">{{$car->fullname}}
+            {{--<div class="pull-right" style="color: blue">4.7分</div>--}}
+        </div>
+
+        <div class="clearfix"></div>
+        <div class="col-xs-5">{{$car->name}}</div><div class="pull-right" style="margin-right: 20px">{{$car->salestate}}</div>
         <div class="clearfix"></div>
 
-        <div class="title col-xs-12">奥迪 A3<div class="pull-right" style="color: blue">4.7分</div></div>
+        {{--<div class="col-xs-2">14.29-29.67万</div><div style="text-decoration:line-through"> 19.05-25.62万</div>--}}
 
-        <div class="clearfix"></div>
-        <div class="col-xs-12">紧凑型车 1.4L/2.0L</div>
-        <div class="clearfix"></div>
-
-        <div class="col-xs-2">14.29-29.67万</div><div style="text-decoration:line-through"> 19.05-25.62万</div>
 
         {{--<div class="category col-xs-10">{{ $article->categories->name }}--}}
                 {{--@if (isset($article->user_created_by->profiles))--}}
@@ -41,7 +44,7 @@
         {{--<div class="clearfix"></div>--}}
         {{--</table>--}}
 
-    </div>
+    {{--</div>--}}
 </div>
 
 <script src="{{ url('/src/js/jQuery.min.2.2.4.js') }}" ></script>
